5b1ab0b679579b95918c0d296bc369fd150f3f5d
Viachaslau Shpileuski <viachaslau.shpileuski@coxautoinc.com>
2021-09-29 18:42:12 +0300

    feat: create course-card and course-list components, implement course-cart component logic from the task

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { InfoComponent } from './components/info/info.component';
 import { SearchComponent } from './components/search/search.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
